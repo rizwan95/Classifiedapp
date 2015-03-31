14173a869b20bce7e2cfa648954a208e566fab4d
Rizwan Ahmed <rizwan.ahmed95@gmail.com>
2015-03-31 20:50:57 +0530

    Webview example

---
+++ Website/app/src/main/java/com/idroidwarz/application/website/MainActivity.java
@@ -0,0 +1,54 @@
+package com.idroidwarz.application.website;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView)findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        WebViewClientImpl webViewClient = new WebViewClientImpl(this);
+        webView.setWebViewClient(webViewClient);
+
+        webView.loadUrl("http://idroidwarz.com/class/");
+
+
+
+    }
+
+
+}
+
+ class WebViewClientImpl extends WebViewClient {
+
+    private Activity activity = null;
+
+    public WebViewClientImpl(Activity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+        if(url.indexOf("idroidwarz.com") > -1 ) return false;
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        activity.startActivity(intent);
+        return true;
+    }
+
+}
